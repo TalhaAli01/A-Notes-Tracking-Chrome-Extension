cbc950d060113b12bc9daebdc4f0b5a71a015007
TalhaAli01 <dont4346@gmail.com>
2023-05-18 22:25:38 +0500

    Added the functionality to Delete and Save Notes
    Need to further add styling and a feature to save the tabs URL in anchor tags

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const historyBtn = document.getElementById('history-btn');
 const deleteBtn = document.getElementById('delete-btn');
 const ol = document.getElementById('ol');
 
-let boolean = false;
-const allData = [];
-
 const creatingObjects = () => () => {
+    const allData = [];
     const inputData = inputEl.value;
     const span = document.createElement('span');
     const li = document.createElement('li');
@@ -38,18 +36,43 @@ const retrievingElements = () => {
 const deletingElements = () => {
     const storageItems = JSON.parse(localStorage.getItem('Input Data'));
     console.log(storageItems);
-    storageItems.forEach(element => {
-        const allElements = element.second;
-        ol.innerHTML = allElements;
-    });
-}
+    const newArray = [];
+    for (let i = 0; i < storageItems.length; i++) {
+        newArray.push(storageItems[i].second);
+    }
+    const lastItem = newArray[newArray.length - 1];
+    ol.innerHTML = lastItem;
+    let elements = ol.children;
+    for (let i = 0; i < elements.length; i++) {
+        let allInputs = elements[i].lastChild;
+        allInputs.addEventListener('click',()=>{
+            if (allInputs.checked === true) {
+                let dadElement = allInputs.parentElement;
+                dadElement.remove();
+                let newElements = [];
+                if (ol.childElementCount === 1 && allInputs.checked === true) {
+                    console.log("We only have 1 span");
+                    newElements.push({
+                        'first': ol.children.innerHTML,
+                        'second': ol.innerHTML,
+                    });
+                    localStorage.setItem('Input Data',JSON.stringify(newElements));  
 
-const testFunc = () => {
-
+                    } else if (ol.childElementCount > 1){
+                        newElements.push({
+                            'first': ol.lastChild.firstChild.innerHTML,
+                            'second': ol.innerHTML,
+                        });
+                        localStorage.setItem('Input Data',JSON.stringify(newElements));                
+                    } else {
+                        localStorage.clear()
+                }
+            }
+        })
+    }
 }
 
 saveInputBtn.addEventListener('click', creatingObjects());
 historyBtn.addEventListener('click', retrievingElements);
 deleteBtn.addEventListener('click', deletingElements);
-deleteBtn.addEventListener('click', testFunc);
 
